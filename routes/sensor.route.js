abd9283eadaa3a6de1a8f2adfc02b5becac9c13e
Manoj bhatt <110338913+developerManoj47@users.noreply.github.com>
2025-02-06 23:37:03 +0530

    Convert PATCH to POST for sensor data

--- routes/sensor.route.js
+++ routes/sensor.route.js
@@ -10,12 +10,12 @@ import {
 } from "../controllers/sensor.controller.js";
 
 // without middleware
-router.route("/sensor/data/:sensor_id").patch(updateSensorData); // body- sensorData , battery (Number)
+router.route("/sensor/data/:sensor_id").post(updateSensorData); // body- sensorData , battery (Number)
 router.route("/sensor").get(getSensorData);
 // With middleware
 router.route("/sensor").post(authMiddleware, adminMiddleware, addNewSensor);
 router
   .route("/sensor/action/:sensor_id")
-  .patch(authMiddleware, adminMiddleware, updateActionState); // body - currentState
+  .post(authMiddleware, adminMiddleware, updateActionState); // body - currentState
 
 export default router;
